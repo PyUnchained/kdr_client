1fa69de5a481a3e0bc269c0a408aeb406e76a0ff
Tatenda. K. Tambo <tatendatambo@gmail.com>
2022-02-17 20:56:46 +0200

    Initial commit of KDR package.

---
+++ kivy_django_restful/applet.py
@@ -0,0 +1,4 @@
+class KDRApplet():
+    """ Applet serves as the interface for interacting with all the
+    underlying Django machinery. """
+    pass
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='kivy-django-restful',
+      version='0.1',
+      description='Kivy-side Restful API Client',
+      author='Tatenda Tambo',
+      author_email='tatendatambo@gmail.com',
+      packages=['kivy_django_restful'],
+      install_requires=['wrapt>=1.12.1', 
+      'pickle-storage @ git+https://github.com/PyUnchained/pickle_storage.git@master']
+      )
